2c82df4abb416a53c6957dc9e15dd5b315c255db
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2021-03-02 21:50:44 +0000

    created git ignore for Api key file

---
+++ part1/currencyApi.py
@@ -0,0 +1,43 @@
+import requests
+
+data = requests.get(https://apilayer.net/api/convert?access_key=YOUR-PERSONAL-API-KEY&from=GBP&to=USD&amount=1, params={key: value}, args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+http://api.currencylayer.com/live
+
+    ? access_key = YOUR_ACCESS_KEY
+    & source = GBP
+    & currencies = USD,AUD,CAD,PLN,MXN
+    & format = 1
+
+
