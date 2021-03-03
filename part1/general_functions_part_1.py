77b45e7c2842fd6d65aef944a72cc53d6bf02b46
JamesRFlynn <jamesrichmondflynn@gmail.com>
2021-03-03 10:56:23 +0000

    part 1 coin calc updates

--- part1/general_functions_part_1.py
+++ part1/general_functions_part_1.py
@@ -13,20 +13,45 @@ uk_coins_dict = {
         "20p": 20,
         "10p": 10,
     }
-us_coins = ["$2", "$1", "50c", "20c", "10c"]
-us_coins_dict = {
-        "$2": 200,
-        "$1": 100,
-        "50c": 50,
-        "20c": 20,
-        "10c": 10,
-    }
-
-#---------------------------------------------------
+#---------------------------------------------------------
 
 
-#---------------------------------------------------
-# Functions: 
+#---------------------------------------------------------
+# Functions for the coin_calculator and multiple_coin_calculator:
+#
+#
+# Gets the user to input the amount of pennies that they want to exchange. Ensures the user
+# inputes a positive integer. This parameter
+# is a list that has a 'min_coin_value' and a 'max_coin_value' as keys. get_penny_amount
+# will ensure that the amount of pennies the user chooses is between these values of 
+# 'min_coin_value' and 'max_coin_value'.
+def get_penny_amount():
+    try:
+        pennies = input("How many pennies do you have? Please enter a positive number: ")
+        pennies = int(pennies)
+        if int(pennies) <= 0:
+            print("This is not a valid amount of pennies.")
+            return -3
+        if int(pennies) == 0:
+            print("You need to have some pennies in order to exchange them!")
+            return -4
+        if int(pennies) > 10000:
+            print("The maximum coin value is set to 10000.")
+            return -5
+    except:
+        print("This is not a valid amount of pennies.")
+        return -5
+    return int(pennies)
+#
+#
+#Calculates the floor of x when divided by y
+def floor_calc(x, y):
+    return int((x / y) // 1)
+#-------------------------------------------------------------
+
+
+#-------------------------------------------------------------
+#Functions that may help you create the list:
 #
 #
 # Allows the user to choose what option they want from a menu. The parameter 'choices' is an
@@ -47,37 +72,10 @@ def option_chooser(choices):
     print()    
     print("Invalid choice. Please try again.")
     return -2
-
-
+#
+#
 # Creates a 'fresh' terminal window
 def spacer():
         for i in range(0,100):
             print()
-
-
-#Calculates the floor of x when divided by y
-def floor_calc(x, y):
-    return int((x / y) // 1)
-
-
-# Gets the user to input the amount of pennies that they want to exchange. Ensures the user
-# inputes a positive integer. This parameter
-# is a list that has a 'min_coin_value' and a 'max_coin_value' as keys. get_penny_amount
-# will ensure that the amount of pennies the user chooses is between these values of 
-# 'min_coin_value' and 'max_coin_value'.
-def get_penny_amount():
-    try:
-        pennies = input("How many pennies do you have? Please enter a positive number: ")
-        pennies = int(pennies)
-        if int(pennies) <= 0:
-            print("This is not a valid amount of pennies.")
-            return -3
-        if int(pennies) == 0:
-            print("You need to have some pennies in order to exchange them!")
-            return -4
-    except:
-        print("This is not a valid amount of pennies.")
-        return -5
-    return int(pennies)
-
 #---------------------------------------------------------
