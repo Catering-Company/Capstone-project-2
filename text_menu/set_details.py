927b4e3662b4e3e4fa73dcd292d7d94b17a233b4
JamesRFlynn <jamesrichmondflynn@gmail.com>
2021-03-04 22:14:02 +0000

    fixed set_details inconsistencies

--- text_menu/set_details.py
+++ text_menu/set_details.py
@@ -64,24 +64,13 @@ def main(config):
         if sub_choice == 2:
             spacer()
             min_coin_value = 0
-            min_coin_value = sub_set_min_coin_value.main()
-            if min_coin_value >= 10000:
-                print("Request denied.")
-                print("You cannot set the minimum coin value to 10000 or larger.")
-            elif min_coin_value <= config["max_coin_value"]:
-                config["min_coin_value"] = min_coin_value
-            else:
-                print("Request denied.")
-                print("That is larger than the maximum coin amount!")
+            min_coin_value = sub_set_min_coin_value.main(config)
+            config["min_coin_value"] = min_coin_value
         if sub_choice ==3:
             spacer()
             max_coin_value = 10000
-            max_coin_value = sub_set_max_coin_value.main()
-            if max_coin_value >= config["min_coin_value"]:  
-                config["max_coin_value"] = max_coin_value
-            else:
-                print("Request denied.")
-                print("That is smaller than the minimum coin amount!")
+            max_coin_value = sub_set_max_coin_value.main(config)
+            config["max_coin_value"] = max_coin_value
         print()
         input("Press enter to return to the sub-menu.")
         spacer()
--- text_menu/sub_set_max_coin_value.py
+++ text_menu/sub_set_max_coin_value.py
@@ -2,24 +2,36 @@
 
 # Gets the user to input a new maximum amount of coins that the user can enter 
 # into the Coin Calulator and Mutiple Coin Calculator
-def max_coin_value():
+def max_coin_value(config):
     try:
         max_coin = input("What is the maximum amount of coins you want the machine to accept? ")
         max_coin = int(max_coin)
         if int(max_coin) < 0:
-            print("The maximum must be at least 1.")
+            print("Request Denied.")
+            print("The maximum must be a positive number!")
+            print()
             return -1
         if int(max_coin) > 10000:
+            print("Request Denied.")
             print("The maximum cannot exceed 10000.")
+            print()
+            return -2
+        if max_coin < config["min_coin_value"]:  
+            print("Request denied.")
+            print("That is smaller than the minimum coin amount!")
+            print()
+            return -3
     except:
         print("Please enter a maximum amount of coins.")
+        print()
         return -3
     return int(max_coin)
 
+
 # Returns the number that the user has inputted, provided that the number is greater than 0. If
 # the number is 0 or less then the user is re-prompted.
-def main():
-    max_coin = max_coin_value()
+def main(config):
+    max_coin = max_coin_value(config)
     while max_coin < 0:
-        max_coin = max_coin_value()
+        max_coin = max_coin_value(config)
     return max_coin
