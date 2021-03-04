4f1a49648eb14b2609aedf6b8027deba004e8410
Grigg1 <jacob_grigg@hotmail.co.uk>
2021-03-04 21:08:44 +0000

    New window open added

---
+++ Part3/open_new_window.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+
+import sys
+
+
+class AnotherWindow(QWidget):
+  
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.w = None
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+        self.w.show()
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec_()
