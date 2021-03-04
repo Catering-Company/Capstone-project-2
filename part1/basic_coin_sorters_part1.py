7c4f6ff986ce36fcc069e00c42309c98b5038ac1
kpan-lang <kirapandya@protonmail.com>
2021-03-04 19:05:27 +0000

    part1

---
+++ part1/basic_coin_sorters_part1.py
@@ -0,0 +1,189 @@
+# very basic coin sorter for part 1
+    
+
+# CONFIG is a constant list of the initial configurations of the program. In this program it cannot be changed
+ 
+CONFIG = {
+    "currency": "POUNDS STERLING",
+    "min_coin_value": 0,
+    "max_coin_value": 10000,
+}
+# Global variables:
+#
+#
+# A useful list and dictionary for UK currency
+uk_coins = ["£2", "£1", "50p", "20p", "10p"]
+uk_coins_dict = {
+        "£2": 200,
+        "£1": 100,
+        "50p": 50,
+        "20p": 20,
+        "10p": 10,
+    }
+#---------------------------------------------------------
+#---------------------------------------------------------
+
+
+#---------------------------------------------------------
+# Functions for the coin_calculator and multiple_coin_calculator:
+#
+#
+# Gets the user to input the amount of pennies that they want to exchange. Ensures the user
+# inputes a positive integer. This parameter
+# is a list that has a 'min_coin_value' and a 'max_coin_value' as keys. get_penny_amount
+# will ensure that the amount of pennies the user chooses is between these values of 
+# 'min_coin_value' and 'max_coin_value'.
+def get_penny_amount():
+    try:
+        pennies = input("How many pennies do you have? Please enter a positive number: ")
+        pennies = int(pennies)
+        if int(pennies) <= 0:
+            print("This is not a valid amount of pennies.")
+            return -3
+        if int(pennies) == 0:
+            print("You need to have some pennies in order to exchange them!")
+            return -4
+        if int(pennies) > 10000:
+            print("The maximum coin value is set to 10000.")
+            return -5
+    except:
+        print("This is not a valid amount of pennies.")
+        return -5
+    return int(pennies)
+#
+# Gets the coin-denomination that the user wants to turn their pennies into. If the user
+# enters anything other than a valid coin-denomination then get_denomination returns 
+# 'incorrect_usage' so that the user can be re-prompted via a while-loop in main().
+def get_denomination():
+    denomination = input("What denomination? £2, £1, 50p, 20p or 10p? ")
+    if denomination in uk_coins:
+        return denomination
+    else:
+        print("Please choose £2, £1, 50p, 20p or 10p.")
+        return "incorrect_usage"
+
+#Calculates the floor of x when divided by y
+def floor_calc(x, y):
+    return int((x / y) // 1)
+
+# Given an amount of pennies and a coin-denomination, coin_exchange prints 
+# the number of said coins you can receive in exchange for your pennies, along
+# with the amount of pennies you will have left. 
+def coin_exchange(pennies, denomination):
+    coin_amount = floor_calc(pennies, uk_coins_dict[f"{denomination}"])
+    penny_remainder = pennies % uk_coins_dict[f"{denomination}"]
+    if penny_remainder == 0:
+        if coin_amount == 1:
+            print(f"You can exchange your pennies for exactly {coin_amount} {denomination} coin.")       
+        if coin_amount > 1:
+            print(f"You can exchange your pennies for exactly {coin_amount} {denomination} coins.")           
+    elif coin_amount == 0:
+        print("You don't have enough pennies to exchange!")
+    elif coin_amount == 1:
+        print(f"You can exchange your pennies for {coin_amount} {denomination} coin with {penny_remainder}p to spare.")
+    else: 
+        print(f"You can exchange your pennies for {coin_amount} {denomination} coins with {penny_remainder}p to spare.")
+    return 0
+
+
+# Gets the coin-denomination that the user wants to exclude. If an incorrect string is 
+# entered then exclude_denomination returns 'incorrect_usage' so that the user can be
+# re-prompted via a while-loop in main(). 
+def exclude_denomination():
+    denomination = input("What denomination do you wish to exclude? £2, £1, 50p, 20p or 10p? ")
+    if denomination in uk_coins:
+        return denomination
+    else:
+        print("Please choose £2, £1, 50p, 20p or 10p.")
+        return "incorrect_usage"
+
+# Calculates the amount of each coin the user can receive for their pennies, prioritising
+# the higher denominations. The results are returned in a list, uk_coins_amounts. Each 
+# entry in the list corresponds to the coin in the same entry of the list uk_coins. E.g
+# uk_coins_amounts[1] corresponds to uk_coins[1], which is £1. uk_coins_amounts is one 
+# element longer than uk_coins, however. The extra (final) element corresponds to the
+# amount of pennies that are left over.
+# The input excluded_denomination takes the denomination that
+# needs to be exluded. E.g calculate(1234, £2) would exclude £2 and uk_coins_amounts would
+# read [0, 12, 0, 1, 1, 4]. 12 £1 coins, 1 20p, 1 10p and 4 pence left over. 
+def calculate(pennies, excluded_denomination):
+    uk_coins_amounts = []
+    for i in range(0, len(uk_coins)):
+        if uk_coins[i] != excluded_denomination:
+            uk_coins_amounts.append(floor_calc(pennies, uk_coins_dict[uk_coins[i]]))
+            pennies -= uk_coins_amounts[i] * uk_coins_dict[uk_coins[i]]
+        else:
+            uk_coins_amounts.append(0) 
+    uk_coins_amounts.append(pennies)
+    return uk_coins_amounts 
+
+# result_print prints out the results of the list that the calculate function produces. 
+# The results are printed in a human-readable format.
+def result_print(uk_coins, uk_coins_amounts):
+    print("Your pennies can be exchanged for ", end = "")
+    for i in range(0, len(uk_coins)):
+        if uk_coins_amounts[i] > 1:
+            print(f"{uk_coins_amounts[i]} {uk_coins[i]} coins, ", end = "")
+        elif uk_coins_amounts[i] == 1:
+            print(f"{uk_coins_amounts[i]} {uk_coins[i]} coin, ", end = "")
+    if uk_coins_amounts[-1] == 0:
+        print("and you'll have no pennies left over!")
+    else:
+        if uk_coins_amounts[-1] != 1:
+            print(f"and you'll have {uk_coins_amounts[-1]} pennies left over!")
+        else:
+            print(f"and you'll have {uk_coins_amounts[-1]} penny left over!")
+
+#main program starts here
+
+def main():
+    #print configuration first
+    print("Here is the current configuration of the program: ")
+    print(CONFIG)
+    print("\n")
+    
+    sorter_choice = input("Please select 1 for the single coin sorter, or 2 for the multiple coin sorter.")
+    #Used for if invalid input entered
+    valid =['1', '2']
+    while sorter_choice != valid:
+
+        if sorter_choice == '1':
+            
+            pennies = get_penny_amount()
+            while pennies < 0:
+                pennies = get_penny_amount()
+            denomination = get_denomination()          
+            while denomination == "incorrect_usage":
+                denomination = get_denomination()
+            coin_exchange(pennies, denomination)
+            #put in currency conversion here?
+
+            #sinplest way to repeat program
+            repeat1 = input("Would you like to sort more pennies? Press n for no, or any key for yes.")
+            
+            if repeat1 == 'n':
+                break
+            
+        if sorter_choice == '2':
+            pennies = get_penny_amount()
+            while pennies < 0:
+                pennies = get_penny_amount()
+            excluded_denomination = exclude_denomination()
+            while excluded_denomination == "incorrect_usage":
+                excluded_denomination = exclude_denomination()
+            uk_coins_amounts = calculate(pennies, excluded_denomination)
+            result_print(uk_coins, uk_coins_amounts)
+            #put in currency conversion here?
+            repeat1 = input("Would you like to sort more pennies? Press n for no, or any key for yes.")
+            if repeat1 == 'n':
+                break
+
+        sorter_choice = input("Please select 1 for the single coin sorter, or 2 for the multiple coin sorter.")
+
+        
+   
+
+            
+
+
+main()
